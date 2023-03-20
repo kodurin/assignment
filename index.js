a7c8d96b5712654ffa7dea8f53b211c2752c94e5
kodurin <nagarjuna.6062@gmail.com>
2023-03-20 11:24:39 +0000

    port number change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = 3000;
+var port = 3050;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
